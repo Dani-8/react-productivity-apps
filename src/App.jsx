32b23f08d883de077ecbbf626e8739ca60f397d5
Dani-8 <daniyalw6190@gmail.com>
2026-02-13 11:28:28 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import './App.css'
 
 import ExpenseTracker from "./components/ExpenseTracker/ExpenseTracker"
+import Todo from "./components/Todo/Todo"
 
 
 function App() {
     return (
-      <ExpenseTracker />
+      <Todo />
     )
 }
 
---
+++ src/apps/Todo/Todo.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react"
+
+
+
+function Todo(){
+    return (
+
+        <h2>todo</h2>
+
+    )
+}
