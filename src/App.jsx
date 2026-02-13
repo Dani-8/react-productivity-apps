dce5c579bb24813bc74dd6f7bad721fe245b7c1d
Dani-8 <daniyalw6190@gmail.com>
2026-02-13 14:42:22 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Routes, Route} from "react-router-dom"
 
 import Header from "./portal/Header"
-import AppCard  from "./portal/AppCard"
+import AppCardsList  from "./portal/AppCardsList"
 
 import ExpenseTracker from "./apps/ExpenseTracker/ExpenseTracker"
 import Todo from "./apps/Todo/Todo"
@@ -11,13 +11,20 @@ import './App.css'
 
 function App() {
     return (
-      <div className='min-h-screen bg-slate-50 p-6 md:p-12'>
-        <div className='max-w-4xl mx-auto'>
-          <Header />
-            <h3 className="text-lg font-bold text-slate-700 mb-6">Select a workspace:</h3>
-          <AppCard />
-        </div>
-      </div>
+      <Routes>
+        <Route path='/' element={
+          <div className='min-h-screen bg-slate-50 p-6 md:p-12'>
+            <div className='max-w-4xl mx-auto'>
+              <Header />
+              <h3 className="text-lg font-bold text-slate-700 mb-6">Select a workspace:</h3>
+              <AppCardsList />
+            </div>
+          </div>
+        }/>
+
+          <Route path="/apps/ExpenseTracker/ExpenseTracker" element={<ExpenseTracker />} />
+          <Route path="/apps/Todo/Todo" element={<Todo />} />
+      </Routes>
     )
 }
 
---
+++ src/portal/AppCardsList.jsx
@@ -0,0 +1,25 @@
+import { Wallet, CheckSquare } from 'lucide-react';
+import AppCard from './AppCard';
+export default function AppCardsList() {
+    return (
+        <div className='grid grid-cols-1 gap-6 md:grid-cols-2'>
+            <AppCard
+                to="/apps/ExpenseTracker/ExpenseTracker"
+                icon={<Wallet size={40} />}
+                iconBg="bg-emerald-50"
+                iconColor="text-emerald-600"
+                title="Expense Tracker"
+                descp="Full control over your budget. Log expenses, analyze spending, and save more."
+            />
+            <AppCard
+                to="/apps/Todo/Todo"
+                icon={<CheckSquare size={32} />}
+                iconBg="bg-indigo-50"
+                iconColor="text-indigo-600"
+                title="Todo App"
+                descp="The ultimate productivity tool. Track tasks, set goals, and get things done."
+            />
+        </div>
+
+    )
+}
